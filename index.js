dc0b20b9bdafa07fea4e2c9340c92b54255ce9cd
AidansBots <68917714+AidansBots@users.noreply.github.com>
2020-08-17 19:41:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ bot.on('message', message =>{
         message.channel.send('I am working!');
     } else if(command === 'ping'){
         message.channel.send('pong!')
-    }
-})
+    } else if(command === 'embed'){
+        const testEmbed = new Discord.MessageEmbed();
+        testEmbed.setTitle('User Info');
+        testEmbed.setDescription(`Username: ${message.author.username}\n ID: ${message.author.id}`);
+        testEmbed.setColor('#34ebc9');
+        testEmbed.setTimestamp();
+        testEmbed.setFooter('Made by Aidan#2917 | Make sure to like and sub!');
+        testEmbed.setImage(`https://p1.hiclipart.com/preview/373/977/320/discord-for-macos-white-and-blue-logo-art.jpg`)
+        message.channel.send(testEmbed);
+}})
 
 bot.login('insert token');
